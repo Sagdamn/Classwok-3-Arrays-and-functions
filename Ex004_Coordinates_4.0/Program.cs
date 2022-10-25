a7544d15bcf4580a1536fef86f7a6fb5d104b84d
Madina <sagdeeva.m.r@mail.ru>
2022-10-26 00:53:49 +0300

    Task 4 added

---
+++ Ex004_Coordinates_4.0/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 22: Напишите программу, которая принимает 
+на вход число (N) и выдаёт таблицу квадратов чисел 
+от 1 до N.
+
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+
+void GetNumb(int number)
+{
+    Console.Write($"{number} ->");
+    for (int i = 1; i <= number; i++)
+    {
+        double square = Math.Pow(i, 2);
+        Console.Write($"{square}, ");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+GetNumb(numb);
