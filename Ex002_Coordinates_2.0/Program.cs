eee9598a6dbecd2a233c197bceb12de20f10367e
Madina <sagdeeva.m.r@mail.ru>
2022-10-26 00:53:03 +0300

    Task 2 added

---
+++ Ex002_Coordinates_2.0/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 18: Напишите программу, которая по заданному номеру 
+четверти, показывает диапазон возможных координат точек в этой 
+четверти (x и y).*/
+
+int GetQuoter(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result <= 4)
+            isCorrect = true;
+        else
+            Console.WriteLine("Введите валидную четверть!\n");
+    }
+
+    return result;
+}
+
+void NumberOfQuoter(int numb)
+{
+    if (numb == 1)
+        Console.WriteLine("X от 0 до + бесконечности, Y от 0 до + бесконечности");
+    if (numb == 2)
+        Console.WriteLine("X от 0 до - бесконечности, Y от 0 до + бесконечности");
+    if (numb == 3)
+        Console.WriteLine("X от 0 до - бесконечности, Y от 0 до - бесконечности");
+    if (numb == 4)
+        Console.WriteLine("X от 0 до + бесконечности, Y от 0 до - бесконечности");
+}
+
+int x = GetQuoter("Введите номер четверти:");
+
+NumberOfQuoter(x);
